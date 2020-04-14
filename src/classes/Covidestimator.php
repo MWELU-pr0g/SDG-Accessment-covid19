977775e98595acd9bbf6594baca32423f0041bd3
MWELU MUTINDA <carol.mweluh@gmail.com>
2020-04-14 11:02:18 +0300

    challenge 4

--- src/classes/Covidestimator.php
+++ src/classes/Covidestimator.php
@@ -139,6 +139,7 @@ class Covidestimator
         }
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+        // final message
         $finalmsg = str_replace("/index.php","",$message);
         file_put_contents($destinationPath . '/' . $file, $finalmsg, FILE_APPEND | LOCK_EX);
         file_put_contents($destinationPath . '/' . $file, "\n", FILE_APPEND | LOCK_EX);
